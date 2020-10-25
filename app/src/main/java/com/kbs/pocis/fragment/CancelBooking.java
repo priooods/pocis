df5c429e35a0f5f61c1ee3080ddf39cab4c18b4c
priooods <priodwisembodo23@gmail.com>
2020-10-26 00:49:45 +0700

    Membuat page OnlineBooking dan viewpager untuk menampilkan fragment_all_bookings.xml dan fragment_cancel_booking.xml

---
+++ app/src/main/java/com/kbs/pocis/fragment/CancelBooking.java
@@ -0,0 +1,25 @@
+package com.kbs.pocis.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.kbs.pocis.R;
+
+public class CancelBooking extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_cancel_booking,container,false);
+
+
+        return view;
+    }
+}
