3d083f890c0fafd0092a3d89ec3e9b8c310e0697
priooods <priodwisembodo23@gmail.com>
2020-10-26 00:43:22 +0700

    Mengubah Konsep Layout dari antar activity menjadi antar fragment dengan tujuan meringankan kerja Applikasi. Pada hal ini screen_dasar.xml dan Screen_Dasar.java menjadi latar dari semua fragment yang ada.

--- app/src/main/java/com/kbs/pocis/activity/Screen_Dasar.java
+++ app/src/main/java/com/kbs/pocis/activity/Screen_Dasar.java
@@ -6,15 +6,19 @@ import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.kbs.pocis.R;
+import com.kbs.pocis.fragment.HomePage;
 
-public class Home extends AppCompatActivity {
+public class Screen_Dasar extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_screen);
+        setContentView(R.layout.screen_dasar);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             getWindow().setStatusBarColor(getResources().getColor(R.color.colorWhite, this.getTheme()));
@@ -24,5 +28,12 @@ public class Home extends AppCompatActivity {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);//  set status text dark
         }
 
+        if (savedInstanceState == null){
+            Fragment homepage = new HomePage();
+            FragmentManager manager = getSupportFragmentManager();
+            FragmentTransaction transaction = manager.beginTransaction();
+            transaction.replace(R.id.framelayout, homepage);
+            transaction.commit();
+        }
     }
 }
